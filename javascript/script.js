efcfe36cb8920430e649fd3b5fb02fb856e2912f
Theo <theobasallaje@gmail.com>
2019-01-12 14:04:07 -0600

    added url function

--- javascript/script.js
+++ javascript/script.js
@@ -3,15 +3,25 @@ var numberfRecordsToRetrieveVal = $("#numberfRecordsToRetrieve").val();
 var startYearVal = $("#startYear").val();
 var endYearVal = $("#endYear").val();
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "d05fcb0f8f6947658a9cbb2855f3419b",
-  'q': searchTermVal,
-  'begin_date': startYearVal,
-  'end_date': endYearVal
-});
+function url(searchTermVal, startYearVal, endYearVal){
+    var params = {
+        'api-key': "a31cb31f563a44e69165b720f77054a7",
+        'q': searchTermVal
+    };
+    if(startYear){
+        params.begin_date = startYearVal + "0101";
+    } 
+    if(endYear){
+        params.end_date = endYearVal + "1231";
+    }
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param(params);
+    console.log(url);
+    return url;
+}
+
 $.ajax({
-  url: url,
+  url: url("shutdown"),
   method: 'GET',
 }).done(function(result) {
   console.log(result);
@@ -20,7 +30,7 @@ $.ajax({
 });
 
 $("#search").on("click", function() {
-
+    $(".container").append(JSON.stringify(result));
 });
 
 
