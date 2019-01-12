a6f9fdcfceaf18123ba48a8c89891e5042051d28
Kyle Laster <kyle.laster@gmail.com>
2019-01-12 12:47:55 -0600

    initial

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+var searchTermVal = $("#searchTerm").val();
+var numberfRecordsToRetrieveVal = $("#numberfRecordsToRetrieve").val();
+var startYearVal = $("#startYear").val();
+var endYearVal = $("#endYear").val();
+
+
+
+$("#search").on("click", function() {
+
+};
+
+
+
+$("#clearResults").on("click", function() {
+
+};
